d6d550a2d359cf8713b3c12fca1310ecfd37d77b
Rodolpho Fernandes <rodolphofernandes@outlook.com.br>
2020-06-20 09:38:38 -0300

    Front-end Desafio ReactJS CRUD integrado com o nodeJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+
+    api.get('repositories').then(response => {     
+      setRepositories(response.data);
+    })
+
+  }, []);
+
+
   async function handleAddRepository() {
-    // TODO
+    
+    const response = await api.post('repositories',{
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Rodolpho Fernandes"
+        });
+
+    const repositorie = response.data;
+
+    //console.log(...repositories);
+
+    setRepositories([...repositories, repositorie]);
+    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    console.log(repositories);
+    
+    const response = await api.delete(`repositories/${id}`);
+    const { status } = response;
+    
+    if(status == 204){
+      const result = repositories.filter(repo => repo.id !== id);
+
+      
+      setRepositories([...result]);
+    }
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+      
+      <ul data-testid="repository-list">
+      { repositories.map(repositorie => 
+        <li key={repositorie.id}>{repositorie.title}
+        <button onClick={() => handleRemoveRepository(repositorie.id)}>
+          Remover
+        </button>
         </li>
+      ) }
+      
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
